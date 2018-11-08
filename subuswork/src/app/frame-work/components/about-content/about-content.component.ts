e5162c8b0fdf3b99d7058abc1e2a6b312c59613a
subramanya.hejamadi <subramanya.hejamadi>
2018-11-08 18:53:58 +0530

    social links implemented

--- subuswork/src/app/frame-work/components/about-content/about-content.component.ts
+++ subuswork/src/app/frame-work/components/about-content/about-content.component.ts
@@ -12,4 +12,26 @@ export class AboutContentComponent implements OnInit {
   ngOnInit() {
   }
 
+  redirectTo(social: string) {
+    switch (social) {
+      case 'instagram':
+        window.open('https://www.instagram.com/subuswork/?hl=en', '_blank');
+        break;
+      case 'facebook':
+        window.open('https://www.facebook.com/subramanya.shenoy', '_blank');
+        break;
+      case 'linkedin':
+        window.open('https://www.linkedin.com/in/subramanya-shenoy-06b3a972/', '_blank');
+        break;
+      case 'youtube':
+        window.open('https://www.youtube.com/channel/UCqSUgNg3IbN0f0TRSyKTkAw', '_blank');
+        break;
+      case 'git':
+        window.open('https://github.com/subramanya4shenoy/subuswork', '_blank');
+        break;
+      default:
+        break;
+    }
+  }
+
 }
