3d271d8bffffc2661396af615b1a2c1ecbb84729
subramanya.hejamadi <subramanya.hejamadi>
2019-01-12 21:41:41 +0530

    set up for element first

--- subuswork/src/app/features/elements/element-dynamic-logo/element-dynamic-logo.component.ts
+++ subuswork/src/app/features/elements/element-dynamic-logo/element-dynamic-logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-element-dynamic-logo',
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElementDynamicLogoComponent implements OnInit {
 
-  constructor() { }
+  private gradients_collection = [
+    '#01051D',
+    '#01051D',
+    '#01051D',
+    '#01051D',
+    '#01051D',
+    '#01051D',
+    '#535F77',
+    '#7E7693',
+    '#D8A5B7',
+    '#b7c9e5',
+    '#ffffce',
+    '#fff',
+    '#fff',
+    '#fff',
+    '#F9F871',
+    '#FFC75F',
+    '#FF9671',
+    '#FF6F91',
+    '#D65DB1',
+    '#845EC2',
+    '#01051D',
+    '#01051D',
+    '#01051D',
+    '#01051D'
+  ];
+  private bg: number;
+  private gradients_collection_bg = [
+    '#01051D',
+    '#535F77',
+    '#7E7693',
+    '#D8A5B7',
+    '#b7c9e5',
+    '#ffffce',
+    '#fff',
+    '#F9F871',
+    '#FFC75F',
+    '#FF9671',
+    '#FF6F91',
+    '#D65DB1',
+    '#845EC2',
+  ];
+  constructor(private element: ElementRef) { }
 
   ngOnInit() {
+    this.bg = 6;
+  }
+
+  changeBG = (val) => {
+      this.bg = val;
   }
 
 }
