fadcb2534ddb3829aa4d163c802ef9b8bc377bea
subramanya.hejamadi <subramanya.hejamadi>
2019-02-09 22:05:47 +0530

    sw on halt

--- subuswork/src/app/app.module.ts
+++ subuswork/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { FrameWorkModule } from './frame-work/frame-work.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+// import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 
@@ -15,8 +15,8 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     AppRoutingModule,
+    // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     AngularFireModule.initializeApp({
       apiKey: 'AIzaSyBi3XOyybCRzwewmrZ1lCqExnVATtgi7oc',
       authDomain: 'subuswork.firebaseapp.com',
